7be0fc9a0e4a6e0372690b8044332cae8c8939e1
AVK <aleks_vk@ukr.net>
2024-01-26 14:58:52 +0200

    Sepia, red and green filters added
    Signed-off-by: AVK <aleks_vk@ukr.net>

--- src/js/main.js
+++ src/js/main.js
@@ -13,39 +13,106 @@ function updateImage() {
 	}
 	originUploadedImg = new SimpleImage(loadInput);
 	printImg(originUploadedImg);
+	// clearingCurrentImg();
 }
 function printImg(img) {
 	img.drawTo(canvas);
-	//console.log(img.getPixel(2,2));
 }
 function changeImage(event) {
+	if(!originUploadedImg) return;
 	const target = event.target;
 	const action = target.closest('button').dataset.filter;
 	doChange(action);
 }
 function doChange(action) {
-	switch(action) {
-		case 'grey':
-			makeGrey();
-			break;
+	if(action === 'clear') {
+		clearing();
+		return;
 	}
-}
-function makeGrey() {
-	if(!originUploadedImg) return;
-	currentImage = new SimpleImage(originUploadedImg.getWidth(), originUploadedImg.getHeight());
+	clearingCurrentImg();
 	for(let pixel of originUploadedImg.values()) {
 		const x = pixel.getX();
 		const y = pixel.getY();
-		const averageColor = makeGreyColor(pixel);
+		let RGBObj = {};
+		switch(action) {
+			case 'grey':
+				RGBObj = makeGrey(pixel);
+				break;
+			case 'sepia':
+				RGBObj = makeSepia(pixel);
+				break;
+			case 'red':
+				RGBObj = makeUnacolor(pixel, 'R');
+				break;
+			case 'green':
+				RGBObj = makeUnacolor(pixel, 'G');
+				break;
+			case 'clear':
+				clearing();
+				break;
+		}
+		if (RGBObj.R === undefined) {
+			console.log("Не визначений фільтр ");
+			throw new Error("Не визначений фільтр");
+			return;
+		}
 		const newPixel = currentImage.getPixel(x, y);
-		newPixel.setRed(averageColor);
-		newPixel.setGreen(averageColor);
-		newPixel.setBlue(averageColor);
-
+		newPixel.setRed(RGBObj.R);
+		newPixel.setGreen(RGBObj.G);
+		newPixel.setBlue(RGBObj.B);
+		newPixel.setAlpha(pixel.getAlpha());
 	}
 	printImg(currentImage);
 }
-function makeGreyColor(pixel) {
+function makeUnacolor(pixel, color) {
+	const averageColor = culculateAverageColor(pixel);
+	const RGBObj = {};
+	const neededColor = culculateNeededColor(averageColor);
+	if(averageColor < 128) {
+		RGBObj.R = 0;
+		RGBObj.G = 0;
+		RGBObj.B = 0;
+		RGBObj[color] = neededColor;
+	} else {
+		RGBObj.R = neededColor;
+		RGBObj.G = neededColor;
+		RGBObj.B = neededColor;
+		RGBObj[color] = 255;
+	}
+	return RGBObj;
+}
+function culculateNeededColor(averageColor) {
+	if(averageColor < 128) {
+		return averageColor * 2;
+	} else return averageColor * 2 - 255;
+}
+function makeSepia(pixel) {
+	const {Rc, Gc, Bc} = {
+		Rc: pixel.getRed(), 
+		Gc: pixel.getGreen(), 
+		Bc: pixel.getBlue()
+	};
+	return {
+		R: Rc * .393 + Gc * .769 + Bc * .189,
+		G: Rc * .349 + Gc * .686 + Bc * .168,
+		B: Rc * .272 + Gc * .534 + Bc * .131
+	}
+}
+function clearing() {
+	printImg(originUploadedImg);
+}
+function clearingCurrentImg() {
+	currentImage = new SimpleImage(originUploadedImg.getWidth(), originUploadedImg.getHeight());
+}
+function makeGrey(pixel) {
+	const averageColor = culculateAverageColor(pixel);
+	return {
+		R: averageColor,
+		G: averageColor,
+		B: averageColor
+	}
+}
+function culculateAverageColor(pixel) {
 	const averageColor = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
 	if(averageColor > 255) {
 		console.log("Не вірний колір");
